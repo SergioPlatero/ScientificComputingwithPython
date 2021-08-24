d9c9db22ae398fc36613f57cc97c953f0cb24a69
SergioPlatero <83132650+SergioPlatero@users.noreply.github.com>
2021-08-23 23:03:50 -0700

    Update fcc_p3.py

--- fcc_p3.py
+++ fcc_p3.py
@@ -223,7 +223,7 @@ def create_spend_chart(category_list):
     return bar_chart
 
 
-
+# The following lines are a series of tests
 food = Category("Food")
 business = Category("Business")
 entertainment = Category("Entertainment")
@@ -236,36 +236,3 @@ business.withdraw(10.99)
 
 
 print(create_spend_chart([business, food, entertainment]))
-print("Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  ")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
